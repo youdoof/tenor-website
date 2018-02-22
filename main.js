a6979cbddaa1e62ba9d7e58d4d0882f502a9abe6
Andrew Brogan <youdoof@gmail.com>
2018-02-22 14:40:28 -0600

    Start Point
    Got a start point to work with. Responsive pages with navigation. Now to create the content and work on changing the color scheme.

---
+++ main.js
@@ -0,0 +1,17 @@
+function toggleShowMusic() {
+    var content = document.getElementById("music-container");
+    if (content.style.display === "block") {
+        content.style.display = "none";
+    } else {
+        content.style.display = "block";
+    }
+}
+
+function toggleShowResources() {
+    var content = document.getElementById("resources-container");
+    if (content.style.display === "block") {
+        content.style.display = "none";
+    } else {
+        content.style.display = "block";
+    }
+}
---
+++ tuning.js
@@ -0,0 +1,6 @@
+// A, A#/Bb, B, C, C#/Db, D, D#/Eb, E, F, F#/Gb, G, G#/Ab
+
+var notes = ["A", "A#/Bb", "B", "C", "C#/Db", "D",
+             "D#/Eb", "E", "F", "F#/Gb", "G", "G#/Ab"];
+
+var octaves = [0, 1, 2, 3, 4, 5, 6];
